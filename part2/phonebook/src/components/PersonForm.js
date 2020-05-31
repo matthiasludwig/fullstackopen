22ef97ca5b1f7be7d067791f9f278157e0a7fb76
Matthias <matthiasludwig@me.com>
2020-05-31 22:47:29 +0200

    part1 phonebook fixed formatting in PersonForm.js

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -3,14 +3,14 @@ import React from 'react';
 const PersonFrom = (props) => {
   return (
     <form>
-    <div>
-      name: <input onChange={props.handleNameChange} value={props.newName} />
-      <br/>
-      number: <input onChange={props.handleNumberChange} value={props.newNumber} />
-    </div>
-    <div>
-      <button onClick={props.handleSubmit} type="submit">add</button>
-    </div>
+      <div>
+        name: <input onChange={props.handleNameChange} value={props.newName} />
+        <br/>
+        number: <input onChange={props.handleNumberChange} value={props.newNumber} />
+      </div>
+      <div>
+        <button onClick={props.handleSubmit} type="submit">add</button>
+      </div>
   </form>
   )
 }
