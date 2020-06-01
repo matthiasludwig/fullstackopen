56bb8ac85185ac67fa48f94ff6f7316742ce484c
Matthias <matthiasludwig@me.com>
2020-06-01 21:33:21 +0200

    part2 countries finished 2.14

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Country = ({ countryInformation }) => {
+    const [weather, setWeather] = useState({});
+
+    const apiKey = `access_key=${process.env.REACT_APP_API_KEY}`;
+    const weatherUrl = "http://api.weatherstack.com/current?";
+    let capital = `query=${countryInformation.capital}`;
+
+    useEffect(() => {
+        axios
+            .get(`${weatherUrl}${apiKey}&${capital}`)
+            .then(response => setWeather(response.data['current']))
+    }, [apiKey, capital]);
+
+    console.log(weather);
+
     return (
         <>
             <h2>{countryInformation.name}</h2>
@@ -15,6 +30,12 @@ const Country = ({ countryInformation }) => {
                 </ul>
             </div>
             <img src={countryInformation.flag} alt={countryInformation.name} width='200px'/>
+            <h3>Weather in {countryInformation.capital}</h3>
+            <div>
+                <p><strong>temperature:</strong> {weather['temperature']} Celcius</p>
+                <img src={weather["weather_icons"]} alt={weather["weather_descriptions"]} />
+                <p><strong>wind:</strong> {weather['wind_speed']} mph direction {weather['wind_dir']}</p>
+            </div>
         </>
     )
 }
