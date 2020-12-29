74c0434638078138d58bc540a70f8273e3da5e70
Matthias <matthiasludwig@me.com>
2020-12-29 22:59:28 +0100

    Wrote totalLikes tests

--- part4/tests/listHelper.test.js
+++ part4/tests/listHelper.test.js
@@ -7,4 +7,99 @@ test("dummy returns one", () => {
     expect(result).toBe(1)
 })
 
-describe("")
+
+describe("totalLikes calculates", () => {
+    // Array of different blog articles
+    const blogs = [
+        { 
+            _id: "5a422a851b54a676234d17f7",
+            title: "React patterns",
+            author: "Michael Chan",
+            url: "https://reactpatterns.com/",
+            likes: 7,
+            __v: 0 
+        },
+        {
+            _id: "5a422aa71b54a676234d17f8",
+            title: "Go To Statement Considered Harmful",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: "5a422b3a1b54a676234d17f9",
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+            __v: 0
+        },
+        {
+            _id: "5a422b891b54a676234d17fa",
+            title: "First class tests",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+            likes: 10,
+            __v: 0
+        },
+        {
+            _id: "5a422ba71b54a676234d17fb",
+            title: "TDD harms architecture",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+            likes: 0,
+            __v: 0
+        },
+        {
+            _id: "5a422bc61b54a676234d17fc",
+            title: "Type wars",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+            likes: 2,
+            __v: 0
+        }
+    ]
+
+    const blogsErrors = [
+        { 
+            _id: "5a422a851b54a676234d17f7",
+            title: "React patterns",
+            author: "Michael Chan",
+            url: "https://reactpatterns.com/",
+            __v: 0 
+        },
+        {
+            _id: "5a422aa71b54a676234d17f8",
+            title: "Go To Statement Considered Harmful",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: "error",
+            __v: 0
+        }
+    ]
+
+    test("zero input", () => {
+        const result = listHelper.totalLikes([])
+
+        expect(result).toBe(0)
+    })
+
+    test("calculates array of one (with 7 likes) correctly", () => {
+        const result = listHelper.totalLikes([blogs[1]])
+
+        expect(result).toBe(7)
+    })
+
+    test("calculates an array of many (total 36) correctly", () => {
+        const result = listHelper.totalLikes(blogs)
+
+        expect(result).toBe(36)
+    })
+
+    test("rejects array with missing 'likes' property", () => {
+        const result = listHelper.totalLikes(blogsErrors)
+
+        expect(result).toBe("Error in blog object format")
+    })
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -7,7 +7,6 @@ const totalLikes = (posts) => {
 }
 
 
-
 module.exports = {
     dummy,
     totalLikes
